34e7c6f0fc30952e1d6fcef448ea2f8836a5d4ca
Hayakawa, Takuya (SIE) <Takuya.Hayakawa@sony.com>
2017-12-06 16:11:00 +0900

    FPS counter を追加。

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FPSCounter : MonoBehaviour {
+
+    public float updateInterval = 0.5f;
+
+    private float accum = 0.0f;
+    private int frames = 0;
+    private float timeLeft = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        timeLeft = updateInterval;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        timeLeft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        frames++;
+
+        if(timeLeft <= 0.0f)
+        {
+            GetComponent<Text>().text = "FPS: " + (accum / frames).ToString("f2");
+            timeLeft = updateInterval;
+            accum = 0.0f;
+            frames = 0;
+        }
+    }
+}
